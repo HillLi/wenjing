b0ccf7be81a17eb8de7e968c9a49fbd67a78f56b
Li <cugb_ljl@126.com>
2019-09-01 23:25:34 +0800

    第一次提交Github
    wenjing.java

---
+++ wenjing.java
@@ -0,0 +1,15 @@
+public class WenJing{
+
+  private String name;
+  private Integer age;
+  private String password;
+  
+  public WenJing(){}
+  
+  public int happy(){
+    System.out.printLn("文静好姑娘")
+  }
+  
+  public static void ga
+
+}
